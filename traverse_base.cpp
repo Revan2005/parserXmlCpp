84dc72735838ac7123f8f68d7691c862da271385
Revan2005 <tomek.biegus@gmail.com>
2015-11-27 16:16:33 +0100

    znajduje jakos baze i ctag, nie mam pewnosci czy nie przeacza nic, bierze tylko te dla ktorych ktorys atrybut lexa ma nazwe disamb nie sprawdzam czy wartosc tego atrybutu = 1

--- traverse_base.cpp
+++ traverse_base.cpp
@@ -15,16 +15,25 @@ int main()
       for (pugi::xml_node chunk = chunkList.first_child(); chunk; chunk = chunk.next_sibling())
         for (pugi::xml_node sentence = chunk.first_child(); sentence; sentence = sentence.next_sibling())
           for (pugi::xml_node tok = sentence.first_child(); tok; tok = tok.next_sibling())
-          {
-              std::cout << "Orth: " << tok.first_child().name() << " " << tok.first_child().text().get();
-              for (pugi::xml_attribute attr = sentence.first_attribute(); attr; attr = attr.next_attribute())
-              {
-                  std::cout << " " << attr.name() << "=" << attr.value() << " " << tok.text();
-              }
+            for(pugi::xml_node lex = tok.first_child(); lex; lex = lex.next_sibling())
+            {
+                //std::cout << tok.first_child().name() << " " << tok.first_child().text().get();
+                for (pugi::xml_attribute attr = lex.first_attribute(); attr; attr = attr.next_attribute())
+                {
+                  //std::cout << *(attr.value()) << std::endl;
+                //  if(attr.name() == "disamb")
+                  std::string napis = "disamb";
+                  std::string nazwa = attr.name();
+                  if(napis == nazwa)
+                    for(pugi::xml_node element = lex.first_child(); element; element = element.next_sibling())
+                      std::cout << element.text().get() << " ";
+                      //std::cout << " " << attr.name() << "=" << *attr.value() << " " << tok.text()
+                    std::cout << std::endl;
+                }
 
-              std::cout << std::endl;
-          }
-          // end::basic[]
+                //std::cout << std::endl;
+            }
+            // end::basic[]
 
     std::cout << std::endl;
 /*
